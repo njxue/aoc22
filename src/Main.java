3a0e8f1b28383b2175fab087753e6a977b503531
njxue <ngjxue@gmail.com>
2022-12-16 12:13:50 +0800

    Add execution time

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         try {
+            long start = System.currentTimeMillis();
             System.out.println(Day15.Part2.solve());
+            long end = System.currentTimeMillis();
+            System.out.println(String.format("Execution time: %ss", (end - start) / 1000));
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
